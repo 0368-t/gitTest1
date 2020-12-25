158bda099c0c082879f4142fb1c40782cba31253
tutu <1194584163@qq.com>
2020-12-25 15:19:07 +0800

    老师类第二次

---
+++ src/com/yidu/bean/Teacher.java
@@ -0,0 +1,19 @@
+package com.yidu.bean;
+
+/**
+ * @author 刘佳佳
+ * @version 1.0
+ * @公司 一度教育
+ * @date 2020/12/24 16:50
+ */
+public class Teacher {
+
+    public static void main(String[] args) {
+        System.out.println("是个老师类");
+    }
+
+    public void ok(){
+        System.out.println("是ok的!");
+    }
+
+}
